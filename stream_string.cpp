09780e29979027251404096f4cdf920e3620de84
CieranW <cieranwong@gmail.com>
2023-09-12 11:21:28 -0700

    Streams and strings
    Incorporating strings into streams. Learning out how to separate and breakdown a string into different components
    
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- stream_string.cpp
+++ stream_string.cpp
@@ -14,7 +14,16 @@ int main()
     int day, year;
 
     instr >> month >> day >> year;
-    cout << month << ", " << day << endl;
+    cout << month << ", " << day << ", " << year << endl;
 
+    string input2 = "(520)234-5678";
+    int num1, num2, num3;
+    char temp1;
+    istringstream inSS(input2);
+
+    cout << inSS.str() << endl;
+    inSS >> temp1 >> num1 >> temp1 >> num2 >> temp1 >> num3;
+
+    cout << num1 << " " << num2 << " " << num3 << endl;
     return 0;
 }
