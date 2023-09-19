024e420fc9449b3ad3e8cd701a15101549eaef05
CieranW <cieranwong@gmail.com>
2023-09-19 11:17:45 -0700

    Changed the functions to include Name at the end in main.cpp
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- inClassActivities/studentClass/main.cpp
+++ inClassActivities/studentClass/main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main()
 {
     Student student1 = Student();
-    cout << student1.GetFirst() << " "
-         << student1.GetLast() << "/"
+    cout << student1.GetFirstName() << " "
+         << student1.GetLastName() << "/"
          << student1.GetGPA() << endl;
     Student student2 = Student("Wilma", "Cheers", 3.75);
-    cout << student2.GetFirst() << " "
-         << student2.GetLast() << "/"
+    cout << student2.GetFirstName() << " "
+         << student2.GetLastName() << "/"
          << student2.GetGPA() << endl;
     return 0;
 }
