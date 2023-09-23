189584d6fc60cff286fc104d225c9c8dbc59a884
CieranW <cieranwong@gmail.com>
2023-09-22 18:14:16 -0700

    Included some comments in main.cpp to better explain the code
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- hw3/hw3_shopCart1/main.cpp
+++ hw3/hw3_shopCart1/main.cpp
@@ -7,10 +7,12 @@ int main()
 {
 
     /* Type your code here */
+    // Variables
     string name;
     int price, quantity;
     ItemToPurchase item1, item2;
 
+    // Input for Item 1 and 2. Stores the data in their respective holders.
     cout << "Item 1\n"
          << "Enter the item name:\n";
     getline(cin, name);
@@ -35,10 +37,12 @@ int main()
 
     item2 = ItemToPurchase(name, price, quantity);
 
+    // Calculates the total cost for each item, and then total cost overall
     int item1Cost = item1.GetPrice() * item1.GetQuantity();
     int item2Cost = item2.GetPrice() * item2.GetQuantity();
     int totalCost = item1Cost + item2Cost;
 
+    // Outputs the name, price, quantity, and total cost per item in a readable format. Lastly prints the overall total cost.
     cout << "TOTAL COST\n";
     cout << item1.GetName() << " " << item1.GetQuantity() << " @ $" << item1.GetPrice() << " = $" << item1Cost << endl;
     cout << item2.GetName() << " " << item2.GetQuantity() << " @ $" << item2.GetPrice() << " = $" << item2Cost << endl;
