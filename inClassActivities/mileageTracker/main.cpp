347dd748e7ce1b5a9a63ee3c1e30df624aeb3ca5
CieranW <cieranwong@gmail.com>
2023-09-27 18:22:04 -0700

    Completed the TODO comments for in class stuff
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- inClassActivities/mileageTracker/main.cpp
+++ inClassActivities/mileageTracker/main.cpp
@@ -9,15 +9,44 @@ int main()
     MileageTrackerNode *lastNode;
     double miles;
     string date;
-    int i;
+    int i, numNodes = 0;
     // Front of nodes list
     headNode = new MileageTrackerNode();
     lastNode = headNode;
     // TODO: Read in the number of nodes
-    // TODO: For the just-read-in number of nodes, read
-    //   	in data and insert into the linked list
-    // TODO: Call the PrintNodeData() method
-    //   	to print the entire linked list
+    cin >> numNodes;
+    // TODO: For the just-read-in number of nodes, read in data and insert into the linked list
+    for (i = 0; i < numNodes; i++)
+    {
+        cin >> miles;
+        cin >> date;
+
+        currNode = new MileageTrackerNode(miles, date);
+        lastNode->InsertAfter(currNode);
+        lastNode = currNode;
+    }
+    // TODO: Call the PrintNodeData() method to print the entire linked list
+    currNode = headNode->GetNext();
+    while (currNode != nullptr)
+    {
+        currNode->PrintNodeData();
+        currNode = currNode->GetNext();
+    }
+
+    cin >> miles;
+    cin >> date;
+    MileageTrackerNode *temp;
+    temp = new MileageTrackerNode(miles, date);
+    currNode = headNode->GetNext();
+    currNode->InsertAfter(temp);
+
+    currNode = headNode->GetNext();
+    while (currNode != nullptr)
+    {
+        currNode->PrintNodeData();
+        currNode = currNode->GetNext();
+    }
+
     // MileageTrackerNode Destructor deletes all following nodes
     delete headNode;
 }
