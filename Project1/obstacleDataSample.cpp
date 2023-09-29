017262b10e8de1fe794449ea5a55c1dcf2e99f44
CieranW <cieranwong@gmail.com>
2023-09-28 20:50:59 -0700

    Completed Step 2: Classify function, assigning each data point a status given the predetermined conditions
    Will perform some test cases with the given .txt files on the next commit in main.cpp
    
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- Project1/obstacleDataSample.cpp
+++ Project1/obstacleDataSample.cpp
@@ -5,12 +5,12 @@
 #include <iomanip>
 #include <string>
 #include <vector>
+#include <cmath>
 
 int ReadFile(string inputFileName, vector<obstacleDataSample> &data)
 {
     ifstream inFile(inputFileName);
     string linetext;
-    int count = 0;
 
     // Check if the file is open
     if (!inFile.is_open())
@@ -33,15 +33,59 @@ int ReadFile(string inputFileName, vector<obstacleDataSample> &data)
         lineString >> tempData.angle;
 
         data.push_back(tempData);
-        count++;
     }
 
     inFile.close();
-    return count;
+    return 0;
 }
 
 void Classify(vector<obstacleDataSample> &data)
 {
+    const double MAX_DIST = 81.0;
+    const double ANGLE_DEFAULT = -M_PI / 2.0;
+
+    for (size_t i = 0; i < data.size(); i++)
+    {
+        // 1st data point of any list is allocated UNDEFINED
+        if (i == 0)
+        {
+            data[i].status = UNDEFINED;
+        }
+
+        // If the distance or angle are equal to the max distance or default angle, status is UNDEFINDED
+        if (data[i].distance == MAX_DIST || data[i].angle == ANGLE_DEFAULT)
+        {
+            data[i].status = UNDEFINED;
+        }
+
+        // Assigns angleDiff (in degrees) the difference between the current and previous data point, also converts radians into degrees.
+        double angleDiff = (data[i].angle - data[i - 1].angle) * (180.0 / M_PI);
+        // Checks to see if the angle difference is within tolerance of 15 degrees, status assigned ANGLE_RESET
+        if (i != 0 && (angleDiff > 15))
+        {
+            data[i].status = ANGLE_RESET;
+        }
+
+        // Assigns distDiff the difference between current and previous data points
+        double distDiff = data[i].distance - data[i - 1].distance;
+        // Checks to see if the distance difference is within tolerance of 0.1 meters, status assigned DISTANCE_RESET
+        if (i != 0 && (distDiff > 0.1))
+        {
+            data[i].status = DISTANCE_RESET;
+        }
+
+        // If both the distance and angle differences are within the tolerance limit (0.1 meters and 15 degrees respectively), status is assigned VALID
+        if (distDiff < 0.1 && angleDiff < 15)
+        {
+            data[i].status = VALID;
+        }
+
+        // If either the distance or angle difference are greater than the tolerance, and the angle or distance are not the max distance or default angle, status is assigned ANGLE_RESET
+        if ((distDiff > 0.1 || angleDiff > 15) && (data[i].angle != ANGLE_DEFAULT || data[i].distance != MAX_DIST))
+        {
+            data[i].status = ANGLE_RESET;
+        }
+    }
 }
 
 void Filter(vector<obstacleDataSample> &data)
