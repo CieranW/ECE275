51bb2bdae288ae576235b53c46657ae6d114961c
CieranW <cieranwong@gmail.com>
2023-09-29 17:55:53 -0700

    I was so stupid, forgot to output the filtered data set to an output file. All fixed now, should be working just fine. I've saved the files how they should be submitted for the test program. Also included are some test runs for two output files. Might be back again to write some comments or fix little things here and there. Almost over the finish line
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -12,52 +12,68 @@ using namespace std;
 
 ///////////////// Before submitting your code
 // 1) uncomment this part since zyBooks use it.
-/*int main(int argc, const char* argv[]) {
+int main(int argc, const char *argv[])
+{
     if (argc != 3)
     {
         std::cout << "./project1 inputFile outputFile" << std::endl;
         return EXIT_FAILURE;
     }
     string inputFileName = argv[1];
-    string outputFileName = argv[2];*/
-///////////////////// uncomment above befofre submitting on zyBooks /////////////////
-// 2) Comment the next 3 lines below
+    string outputFileName = argv[2];
+    ///////////////////// uncomment above befofre submitting on zyBooks /////////////////
+    // 2) Comment the next 3 lines below
 
-int main(void)
-{
-    string inputFileName = "inputFile9.txt";   // Do NOT change the name "inputFileName" since used above
-    string outputFileName = "outputFile9.txt"; // Do NOT change the name "outputFileName" since used above
+    // int main(void)
+    // {
+    //     string inputFileName = "inputFile9.txt";   // Do NOT change the name "inputFileName" since used above
+    //     string outputFileName = "outputFile9.txt"; // Do NOT change the name "outputFileName" since used above
 
     // Add your code //
     // Variables
     vector<obstacleDataSample> data;
 
     ReadFile(inputFileName, data);
-    for (size_t i = 0; i < data.size(); i++)
-    {
-        cout << fixed << setprecision(6);
-        cout << data[i].timestamp << " " << data[i].distance << " " << data[i].angle << endl;
-    }
-    cout << endl
-         << endl;
+
+    // For testing purposes
+    // for (size_t i = 0; i < data.size(); i++)
+    // {
+    //     cout << fixed << setprecision(6);
+    //     cout << data[i].timestamp << " " << data[i].distance << " " << data[i].angle << endl;
+    // }
+    // cout << endl
+    //      << endl;
 
     Classify(data);
-    for (size_t i = 0; i < data.size(); i++)
-    {
-        cout << fixed << setprecision(6);
-        cout << data[i].timestamp << " " << data[i].distance << " " << data[i].angle << " " << data[i].status << endl;
-    }
 
-    cout << endl
-         << endl;
+    // For testing purposes
+    // for (size_t i = 0; i < data.size(); i++)
+    // {
+    //     cout << fixed << setprecision(6);
+    //     cout << data[i].timestamp << " " << data[i].distance << " " << data[i].angle << " " << data[i].status << endl;
+    // }
 
+    // cout << endl
+    //      << endl;
+
+    // Filters the data and reclassifies it
     Filter(data);
+
+    // Prints the data out to a new file
+    ofstream outFile;
+    outFile.open(outputFileName);
+    outFile << fixed << setprecision(6);
     for (size_t i = 0; i < data.size(); i++)
     {
         obstacleDataSample tempData = data[i];
         string output;
         output = printSample(tempData);
-        cout << output << endl;
+
+        // Write to file
+        outFile << output << endl;
+
+        // For testing purposes
+        // cout << output << endl;
     }
     return 0;
 }
