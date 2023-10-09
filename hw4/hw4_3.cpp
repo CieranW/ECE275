c7d58e14eea9ed1bd22b40acd7e368b6008f2d7a
CieranW <cieranwong@gmail.com>
2023-10-08 19:24:16 -0700

    Completed part 3 but I'm not sure why the unit test on ZyBooks doesn't think that the function returns an exception. Still runs everything else.
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- hw4/hw4_3.cpp
+++ hw4/hw4_3.cpp
@@ -4,11 +4,32 @@
 using namespace std;
 
 /* Define your function here */
+double StepsToMiles(int userSteps)
+{
+    if (userSteps < 0)
+    {
+        throw invalid_argument("Exception: Negative step count entered.");
+    }
+    return static_cast<double>(userSteps) / 2000;
+}
 
 int main()
 {
 
     /* Type your code here. */
+    int userSteps;
+    double userMiles;
 
+    cin >> userSteps;
+
+    try
+    {
+        userMiles = StepsToMiles(userSteps);
+        cout << fixed << setprecision(2) << userMiles << endl;
+    }
+    catch (invalid_argument &e)
+    {
+        cout << e.what() << endl;
+    }
     return 0;
 }
