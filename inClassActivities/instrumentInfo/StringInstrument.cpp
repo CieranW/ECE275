f025b717c8820e6004d8767248ff49f2206c0018
CieranW <cieranwong@gmail.com>
2023-10-10 11:11:37 -0700

    Init commit for today's in class stuff. Initial commit has all the provided base code. Will include a .txt file with the instructions in the next commit
    Signed-off-by: CieranW <cieranwong@gmail.com>

---
+++ inClassActivities/instrumentInfo/StringInstrument.cpp
@@ -0,0 +1,7 @@
+#include "StringInstrument.h"
+
+// TODO: Define mutator functions -
+//      SetNumOfStrings(), SetNumOfFrets(), SetIsBowed()
+
+// TODO: Define accessor functions -
+//      GetNumOfStrings(), GetNumOfFrets(), GetIsBowed()
---
+++ inClassActivities/instrumentInfo/StringInstrument.h
@@ -0,0 +1,17 @@
+#ifndef STR_INSTRUMENTH
+#define STR_INSTRUMENTH
+
+#include "Instrument.h"
+
+class StringInstrument : public Instrument
+{
+    // TODO: Declare private data members: numStrings, numFrets, isBowed
+
+    // TODO: Declare mutator functions -
+    //      SetNumOfStrings(), SetNumOfFrets(), SetIsBowed()
+
+    // TODO: Declare accessor functions -
+    //      GetNumOfStrings(), GetNumOfFrets(), GetIsBowed()
+};
+
+#endif
