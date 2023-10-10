f8e40e21630a48bfe0c9232b6046b147f6848177
CieranW <cieranwong@gmail.com>
2023-10-10 11:25:46 -0700

    Completed TODO comments in StringInstrument.cpp and .h. Everything ran well in ZyBooks, passed all the tests
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- inClassActivities/instrumentInfo/StringInstrument.cpp
+++ inClassActivities/instrumentInfo/StringInstrument.cpp
@@ -1,7 +1,28 @@
 #include "StringInstrument.h"
 
-// TODO: Define mutator functions -
-//      SetNumOfStrings(), SetNumOfFrets(), SetIsBowed()
-
-// TODO: Define accessor functions -
-//      GetNumOfStrings(), GetNumOfFrets(), GetIsBowed()
+// TODO: Define mutator functions - SetNumOfStrings(), SetNumOfFrets(), SetIsBowed()
+void StringInstrument::SetNumOfStrings(int userNumStrings)
+{
+    numStrings = userNumStrings;
+}
+void StringInstrument::SetNumOfFrets(int userNumFrets)
+{
+    numFrets = userNumFrets;
+}
+void StringInstrument::SetIsBowed(bool userIsBowed)
+{
+    isBowed = userIsBowed;
+}
+// TODO: Define accessor functions - GetNumOfStrings(), GetNumOfFrets(), GetIsBowed()
+int StringInstrument::GetNumOfStrings()
+{
+    return numStrings;
+}
+int StringInstrument::GetNumOfFrets()
+{
+    return numFrets;
+}
+bool StringInstrument::GetIsBowed()
+{
+    return isBowed;
+}
--- inClassActivities/instrumentInfo/StringInstrument.h
+++ inClassActivities/instrumentInfo/StringInstrument.h
@@ -6,12 +6,20 @@
 class StringInstrument : public Instrument
 {
     // TODO: Declare private data members: numStrings, numFrets, isBowed
+private:
+    int numStrings;
+    int numFrets;
+    bool isBowed;
 
-    // TODO: Declare mutator functions -
-    //      SetNumOfStrings(), SetNumOfFrets(), SetIsBowed()
-
-    // TODO: Declare accessor functions -
-    //      GetNumOfStrings(), GetNumOfFrets(), GetIsBowed()
+    // TODO: Declare mutator functions - SetNumOfStrings(), SetNumOfFrets(), SetIsBowed()
+public:
+    void SetNumOfStrings(int userNumStrings);
+    void SetNumOfFrets(int userNumFrets);
+    void SetIsBowed(bool userIsBowed);
+    // TODO: Declare accessor functions - GetNumOfStrings(), GetNumOfFrets(), GetIsBowed()
+    int GetNumOfStrings();
+    int GetNumOfFrets();
+    bool GetIsBowed();
 };
 
 #endif
