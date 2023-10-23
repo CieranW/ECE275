6dc07732d84cd42b87b7a27b1ee4b9e38080882f
CieranW <cieranwong@gmail.com>
2023-10-23 12:33:42 -0700

    Completed the TODO comments. Everything ran well on ZyBooks. Had to adjust the cin lines to getline to account for items that may be multiple words like: ice cream or orange juice
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- hw6/hw6_1/main.cpp
+++ hw6/hw6_1/main.cpp
@@ -7,14 +7,26 @@ using namespace std;
 
 int main () {
    // TODO: Declare a list called shoppingList of type ListItem
-
+   list<ListItem> shoppingList;
    string item;
 
    // TODO: Read inputs (items) and add them to the shoppingList list
    //       Read inputs until a -1 is input
+   getline(cin, item);
+   while (item != "-1") {
+      shoppingList.push_back(ListItem(item));
+      getline(cin, item);
+   }
 
 
    // TODO: Print the shoppingList list using the PrintNodeData() function
 
+   for (list<ListItem>::iterator it = shoppingList.begin(); it != shoppingList.end(); ++it) {
+      it->PrintNodeData();
+   }
+
    return 0;
 }
+
+// To run program
+//g++ -c main.cpp -o main.o && g++ -c ListItem.cpp -o ListItem.o && g++ main.o ListItem.o -o my_program && ./my_program
