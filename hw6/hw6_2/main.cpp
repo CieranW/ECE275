687109eca3d6447c2096de808fae65d5da232174
CieranW <cieranwong@gmail.com>
2023-10-23 16:41:16 -0700

    Completed the TODO comments. The second TODO took a lot longer to figure out, turns out I just needed to shift around some stuff within the while loop for it to correctly output the stuff in the right order.
    Signed-off-by: CieranW <cieranwong@gmail.com>

--- hw6/hw6_2/main.cpp
+++ hw6/hw6_2/main.cpp
@@ -5,15 +5,19 @@ using namespace std;
 
 int main () {
    string personName = "";
-   int counter = 0;
+   int counter = 1;
    int youPosition;
 
    queue<string> peopleInQueue;
 
    getline(cin, personName);
    while (personName != "-1") {
-      // TODO: Add personName to peopleInQueue
-      //       determine position of "You" (youPosition)
+      // TODO: Add personName to peopleInQueue determine position of "You" (youPosition)
+      peopleInQueue.push(personName);
+      if (personName == "You") {
+         youPosition = counter;
+      }
+      counter++;
 
       getline(cin, personName);
    }
@@ -21,10 +25,20 @@ int main () {
    cout << "Welcome to the ticketing service... " << endl;
    cout << "You are number " << youPosition << " in the queue." << endl;
 
-   // TODO: In a loop, remove head person from peopleInQueue,
-   //       output their name and that they have purchased a ticket,
-   //       then output your position in the queue. When you are at
-   //       the head, output that you can purchase your ticket.
+   // TODO: In a loop, remove head person from peopleInQueue, output their name and that they have purchased a ticket, then output your position in the queue. When you are at the head, output that you can purchase your ticket.
+
+   while (!peopleInQueue.empty()) {
+      youPosition--;
+      if (youPosition == 0) {
+         cout << "You can now purchase your ticket!" << endl;
+         break;
+      }
+      else {
+      cout << peopleInQueue.front() << " has purchased a ticket." << endl;
+         cout << "You are now number " << youPosition << endl;
+      }
+      peopleInQueue.pop();
+   }
 
 
    return 0;
